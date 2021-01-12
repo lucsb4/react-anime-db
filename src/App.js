901d021420fc05937aefb3537ee94a59b372adc7
Lucas <lucasbarbosa@outlook.com>
2021-01-12 19:26:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Header from './Components/Header';
+import MainContent from './Components/MainContent';
+import Sidebar from './Components/Sidebar';
 
 function App() {
+
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getTopAnime = async () => {
+    const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+    .then(res => res.json());
+
+    setTopAnime(temp.top.slice(0, 5));
+  } 
+
+  const fetchAnime = async (query) => {
+    const temp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`)
+    .then(res => res.json());
+
+    setAnimeList(temp.results);
+  }
+  
+  const handleSearch = e => {
+    e.preventDefault();
+
+    fetchAnime(search);
+  }
+
+  useEffect(() => {
+    getTopAnime();
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent 
+          handleSearch={handleSearch}
+          search={search}
+          setSearch={setSearch}
+          animeList={animeList}
+        />
+      </div>
     </div>
   );
 }
